26960cf808f796611105f665510c4f9f065fd741
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-04-07 11:51:51 +0200

    InitCommit just tests

---
+++ Tpinfo4/Morpion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tpinfo4 {
+  public class Morpion {
+
+    public int NRows { get; set; }
+    public int NCollumns { get; set; }
+    /// <summary>
+    /// Character representing PC player 
+    /// </summary>
+    public Char PCChar { get; set; }  
+    /// <summary>
+    /// Character representing Human player
+    /// </summary>
+    public Char USerChar { get; set; }
+
+    /// <summary>
+    /// Default constructor: Creates a Morpion board with defualt size of 3x3;
+    /// </summary>
+
+    public Morpion() {
+      NRows = 3;
+      NCollumns = 3;
+    }
+    /// <summary>
+    /// Creates a Morpion Board with custom size;
+    /// </summary>
+    /// <param name="nrows"><value>Number of rows in the board</value></param>
+    /// <param name="ncolumns"><value>Number of collumns in the board</value></param>
+
+    public Morpion(int nrows, int ncolumns) {
+      NRows = nrows;
+      NCollumns = ncolumns;
+    }
+
+
+
+    #region Methods
+
+
+
+
+
+
+
+
+    #endregion
+
+
+
+
+
+
+  }
+
+
+}
--- Tpinfo4/Program.cs
+++ Tpinfo4/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tpinfo4
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
+namespace Tpinfo4 {
+  class Program {
+    static void Main(string[] args) {
+
+      Console.BackgroundColor = ConsoleColor.Red;
+      Console.WriteLine("----------------");
+      Console.WriteLine(" X | X | X | X |");
+      Console.WriteLine("---+---+---+----");
+      Console.WriteLine(" X | X | X | X |");
+      Console.WriteLine("---+---+---+----");
+      Console.WriteLine(" X | X | O | O |");
+      Console.WriteLine("---+---+---+----");
+
+
+
     }
+
+
+
+
+
+
+  }
 }
