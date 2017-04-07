4df9c022381a17f7c8ad5511794101ff5d46d0b2
MarouenKanoun <marouen1931@hotmail.com>
2017-04-07 10:06:02 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,13 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace morpion_vendredi
-{
-    class Program
+  class Program
     {
         static void Main(string[] args)
 
 
         {
+
             int b; // afficher le premier nombre aletoire x ou o 
             int i = 0; //  compteur des ligne
             int j = 0;// compteur des colone 
@@ -22,20 +22,21 @@ namespace morpion_vendredi
             char jetonutulisateur = 'o';
             char jetonordinateur = 'x';
             bool bingo = false;
+            //int placerestant = 9; 
 
 
             ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            // methodes permet de donner un tour a chacun 
+         // donner un tour a chacun 
             /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            int parti = 0;
+            int parti = 0;           // variable parti qui s'incremente de 1 a chaque tour  pour  determiner si la partie est paire ou impaire 
             do
             {
+                int placerestant = 9;    // place restant permet d'arreter le jeu si il reste plus de case vide 
                 char[,] Morpion = new char[3, 3]; // matrice du morpion 
-                if (parti % 2 == 0)
+                if (parti % 2 == 0)    // si la partie est paire 
 
                 {
-
                     ee: try
 
                     {
@@ -60,10 +61,7 @@ namespace morpion_vendredi
                     {
                         jetonordinateur = 'o';
                     }
-                    if (jetonutulisateur == 'o')
-                    {
-                        jetonordinateur = 'x';
-                    }
+                   else { jetonordinateur = 'o' ;  jetonutulisateur = 'x';  }
 
                     Console.WriteLine("l'utulisateur : moi je choisit de jouer en {0} ", jetonutulisateur);
                     Console.WriteLine("l'ordinateur : Ok ! moi je prend les  {0} alors..mais la prochaine fois c'est moi qui choisit ", jetonordinateur);
@@ -77,8 +75,8 @@ namespace morpion_vendredi
 
                     do
                     {
-                        Random alea = new Random();
-                        b = alea.Next(110, 122);
+                        Random alea = new Random();    
+                        b = alea.Next(110, 122);               // un systeme aleatoire permet de choisir x ou o  en passant par leur codes ascii 
                     }
 
                     while (b != 111 && b != 120);
@@ -87,7 +85,7 @@ namespace morpion_vendredi
 
                     if (jeuordi == 'x')
                     {
-                        jetonutulisateur = 'o';
+                        jetonutulisateur = 'o';                     // les jeton doivent etre differents
                     }
                     if (jeuordi == 'o')
                     {
@@ -97,35 +95,102 @@ namespace morpion_vendredi
                     Console.WriteLine("ordinateur : je choisi de jouer les  {0} ", jeuordi);
                     Console.WriteLine("utulisateur : d'accord ! je prend les alors {0} ", jetonutulisateur);
 
-                    jeuordi = jetonordinateur;
+                    jeuordi = jetonordinateur; 
+
                     ///// chacun a choisi c'est jeton /////////////////////////////////////////////
                 }
 
                 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                 // l'utulisteur choisi une position pour sont jeton (methode choix de position ) 
-
-                do
+                if (placerestant > 0)
                 {
-                    ll: try
+                    do
                     {
-                        Console.WriteLine("choisissez la position de votre jeton ");
-
-                        Console.WriteLine("ligne= ");
-                        i = Convert.ToInt32(Console.ReadLine());
-
-                        Console.WriteLine("colone= ");
-                        j = Convert.ToInt32(Console.ReadLine());
-
-                        if (Morpion[i, j] != 'x' && Morpion[i, j] != 'o')
+                        ll: try
                         {
-                            Morpion[i, j] = jetonutulisateur;
+                            Console.WriteLine("choisissez la position de votre jeton ");
+
+                            Console.WriteLine("ligne= ");
+                            i = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("colone= ");
+                            j = Convert.ToInt32(Console.ReadLine());
+
+                            if (Morpion[i, j] != 'x' && Morpion[i, j] != 'o')
+                            {
+                                Morpion[i, j] = jetonutulisateur;
+                                placerestant--;
+                             
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("ordinateur :  ah non ! mais cette case est deja occupé ");
+                                goto ll;
+                            }
+
+
+
+                            for (i = 0; i < 3; i++)
+                            {
+                                for (j = 0; j < 3; j++)
+                                {
+                                    Console.Write("{0}", Morpion[i, j]);
+                                }
+                                Console.WriteLine();
+                            }
                         }
-
-                        else
+                        catch (Exception k)
                         {
-                            Console.WriteLine("ordinateur :  ah non ! mais cette case est deja occupé ");
+
+                            Console.WriteLine(k.Message);
                             goto ll;
                         }
+
+
+
+
+                        //////////////// c'est a l'odi de placer sont jeton ////////////////
+
+                        if (placerestant > 0)
+                        {
+                            dd: do
+                            {
+
+
+                                Random lig = new Random();
+                                Random col = new Random();
+
+                                c = lig.Next(0, 3);
+                                l = col.Next(0, 3);
+
+                            }
+
+                            while (l != 0 && l != 1 && l != 2 && c != 0 && c != 1 && c != 2);
+
+                            if (Morpion[l, c] != 'x' && Morpion[l, c] != 'o')
+                            {
+                                Morpion[l, c] = jetonordinateur;
+                                placerestant--;
+
+                                
+                            }
+
+                            else
+                            {
+                                goto dd;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("match null ");
+                            break;
+                        }
+
+
+                        Console.WriteLine(" ordinateur : Ok! bien joué moi je joue les position :");
+                        Console.WriteLine("");
+                        Console.WriteLine(" la colone {0} et la ligne {1}  ", c, l);
 
 
 
@@ -137,65 +202,15 @@ namespace morpion_vendredi
                             }
                             Console.WriteLine();
                         }
-                    }
-                    catch (Exception k)
-                    {
 
-                        Console.WriteLine(k.Message);
-                        goto ll;
-                    }
-
-
-
-                    //////////////// c'est a l'odi de placer sont jeton ////////////////
-
-
-                    dd: do
-                    {
-
-                        Random lig = new Random();
-                        Random col = new Random();
-
-                        c = lig.Next(0, 3);
-                        l = col.Next(0, 3);
-
-                    }
-
-                    while (l != 0 && l != 1 && l != 2 && c != 0 && c != 1 && c != 2);
-
-                    if (Morpion[l, c] != 'x' && Morpion[l, c] != 'o')
-                    {
-                        Morpion[l, c] = jetonordinateur;
-                    }
-
-                    else
-                    {
-                        goto dd;
-                    }
-                    Console.WriteLine(" ordinateur : Ok! bien joué moi je joue les position :");
-                    Console.WriteLine("");
-                    Console.WriteLine(" la colone {0} et la ligne {1}  ", c, l);
-
-
-
-
-                    for (i = 0; i < 3; i++)
-                    {
-                        for (j = 0; j < 3; j++)
+                        for (i = 0; i < 3; i++)
                         {
-                            Console.Write("{0}", Morpion[i, j]);
-                        }
-                        Console.WriteLine();
-                    }
-
-                    for (i = 0; i < 3; i++)
-                    {
 
                             if (Morpion[i, 0] == Morpion[i, 1] && Morpion[i, 1] == Morpion[i, 2] && Morpion[i, 1] == 'o')
                             {
-                            bingo = true;
-                            Console.WriteLine("ok !! c'est une ligne ");
-                            break;
+                                bingo = true;
+                                Console.WriteLine("ok !! c'est une ligne ");
+                                break;
                             }
 
                             if (Morpion[i, 0] == Morpion[i, 1] && Morpion[i, 1] == Morpion[i, 2] && Morpion[i, 1] == 'x')
@@ -203,42 +218,45 @@ namespace morpion_vendredi
                                 bingo = true;
                                 Console.WriteLine("ok !! c'est une ligne ");
                             }
-                          
-                             if (Morpion[0, i] == Morpion[1, i] && Morpion[1, i] == Morpion[2, i] && Morpion[1, i] == 'o')
-                            {
-                                    bingo = true;
-                                    Console.WriteLine("ok !! c'est une ligne ");
-                            }
-                               
-                              if (Morpion[0, i] == Morpion[1, i] && Morpion[1, i] == Morpion[2, i] && Morpion[1, i] == 'x')
-                            {
-                                    bingo = true;
-                                    Console.WriteLine("ok !! c'est une ligne ");
-                            }
-                                
-                                if (Morpion[0, 0] == Morpion[1, 1] && Morpion[1, 1] == Morpion[2, 2] && Morpion[1,1]=='x' )
-                            {
-                                    bingo = true;
-                                    Console.WriteLine("ok !! c'est une ligne ");
-                            }
-                                if
-                                        (Morpion[0, 2] == Morpion[1, 1] && Morpion[1, 1] == Morpion[2, 0] && Morpion[1,1]=='o')
-                            {
-                                    bingo = true;
-                                    Console.WriteLine("ok !! c'est une ligne ");
-                             }
 
+                            if (Morpion[0, i] == Morpion[1, i] && Morpion[1, i] == Morpion[2, i] && Morpion[0, i] == 'o')
+                            {
+                                bingo = true;
+                                Console.WriteLine("ok !! c'est une ligne ");
                             }
 
-                      }
-                while (bingo == false);
-                parti++;
+                            if (Morpion[0, i] == Morpion[1, i] && Morpion[1, i] == Morpion[2, i] && Morpion[0, i] == 'x')
+                            {
+                                bingo = true;
+                                Console.WriteLine("ok !! c'est une ligne ");
+                            }
 
-            }
+                            if (Morpion[0, 0] == Morpion[1, 1] && Morpion[1, 1] == Morpion[2, 2] && Morpion[1, 1] == 'x')
+                            {
+                                bingo = true;
+                                Console.WriteLine("ok !! c'est une ligne ");
+                            }
+                            if
+                                    (Morpion[0, 2] == Morpion[1, 1] && Morpion[1, 1] == Morpion[2, 0] && Morpion[1, 1] == 'o')
+                            {
+                                bingo = true;
+                                Console.WriteLine("ok !! c'est une ligne ");
+                            }
+
+                        }
+                    }
+                    while (bingo == false);
+                    parti++;
+                }
+                else
+                {
+                    Console.WriteLine("match null" );
+                    break; 
+                }
+            } 
+
             while (parti < 5);
-
         }
-
 
     }
 }
